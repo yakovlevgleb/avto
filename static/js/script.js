8ad435f66f20812f693fe9d66033c72a0017792e
Yakovlev Gleb <brin@ya.ru>
2019-05-27 09:43:14 +0500

    init

---
+++ static/js/script.js
@@ -0,0 +1,95 @@
+﻿$(document).ready(function() {
+
+	window.reginas = {};
+
+	window.reginas.slider = ({
+		init: function () {
+			var heroSlider = new Swiper('.js-hero-slider', {
+				speed: 400,
+				navigation: {
+					nextEl: '.js-hero-slider .swiper-button-next',
+					prevEl: '.js-hero-slider .swiper-button-prev',
+				},
+				pagination: {
+					el: '.js-hero-slider .swiper-pagination',
+					type: 'bullets',
+					clickable: true
+				},
+			});
+
+
+			var catalogItemSlider = new Swiper('.js-catalog-slider', {
+				speed: 400,
+				spaceBetween: 30,
+				pagination: {
+					el: '.js-catalog-slider .swiper-pagination',
+					type: 'bullets',
+					clickable: true
+				},
+			});
+
+		}
+	}).init();
+
+	window.reginas.dotdotdot = ({
+		init: function () {
+			$(".inews__text").dotdotdot({
+				height: 65
+			});
+
+			$(".ireviews__text").dotdotdot({
+				height: 110
+			});
+		}
+	}).init();
+
+	window.reginas.styler = ({
+		init: function () {
+			$('select').styler();
+		}
+	}).init();
+
+	window.reginas.burger = ({
+		init: function () {
+			$('.js-burger').on('click', function () {
+				var _t = $(this),
+					nav = _t.siblings('.nav'),
+					body = $('body');
+				if (!_t.hasClass('header__burger--active')) {
+					_t.addClass('header__burger--active');
+					body.addClass('ovh');
+					nav.fadeIn('350');
+				} else {
+					_t.removeClass('header__burger--active');
+					body.removeClass('ovh');
+					nav.fadeOut('350');
+				}
+				return false;
+			});
+		}
+	}).init();
+
+	window.reginas.fancy = ({
+		init: function () {
+			$("[data-fancybox]").fancybox({
+				lang: 'ru',
+				touch: false,
+				i18n: {
+					'ru': {
+						CLOSE: 'Закрыть',
+						NEXT: 'Следующая',
+						PREV: 'Предыдущая',
+						ERROR: 'Запрошенный контент не может быть загружен. <br> Повторите попытку позже.',
+						PLAY_START: 'Начать слайд-шоу',
+						PLAY_STOP: 'Остановить слайд-шоу',
+						FULL_SCREEN: 'Полный экран',
+						THUMBS: 'Эскизы',
+						DOWNLOAD: 'Скачать',
+						SHARE: 'Поделиться',
+						ZOOM: 'Увеличить'
+					}
+				}
+			});
+		}
+	}).init();
+});
